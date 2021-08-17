b967c98d221ef7778f467b975efc7dc68bb17965
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2021-08-17 13:48:02 -0400

    Added simple tested script

---
+++ slack.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import os.path
+import os
+import datetime
+from tabulate import tabulate
+from argparse import ArgumentParser
+import warnings
+from warnings import warn as warning
+
+parser = ArgumentParser()
+parser.add_argument( '-m', '--message', dest='message', default=None, help='Message')
+parser.add_argument( '-c', '--channel', dest='channel', default=None, help='Channel to message')
+results = parser.parse_args()
+
+# read secrets. assumes file exists in ~/.SLACK_SECRETS
+from pathlib import Path
+file = str(Path.home()) + os.sep + '.SLACK_SECRETS'
+exec(open(file).read())
+
+if results.channel == 'icaoberg':
+	url = CHANNEL_ICAOBERG
+elif results.channel == 'bil-dev':
+	url = CHANNEL_BIL_DEV
+else:
+	print('Unknown channel name. Exiting method.')
+	exit()
+
+command = 'curl -X POST -H \'Content-type: application/json\' --data \'{"text":"' + results.message + '"}\' ' + url
+os.system(command)
+exit()
